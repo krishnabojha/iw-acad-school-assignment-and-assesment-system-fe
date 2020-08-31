8a1e8d37c048f1459ee9ad370d0caa06408daf81
krishnabojha <ojhakrishna010@gmail.com>
2020-08-31 20:41:34 +0545

    [created] reset password using mailtrap in backend

---
+++ final-project-react/src/Authentication/ResetPassword.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import './UserLogin.css'
+
+export default class UserLogin extends Component {
+    state = {
+        otppassword: '',
+        newpassword: '',
+        confirmpassword: '',
+        OTPcode:''
+    }
+    componentDidMount = () =>{
+        //request backend to send OTP code to the mail
+        const min =1000;
+        const max = 9999;
+        const rand = min + Math.random() * (max - min);
+        const OTPdata = new FormData()
+        OTPdata.append('email', localStorage.getItem('email'))
+        OTPdata.append('otpcode', parseInt(rand))
+        console.log('userid', localStorage.getItem('UserId'))
+        this.setState({
+            OTPcode: parseInt(rand)
+        })
+        fetch('http://127.0.0.1:8000/user/send_otp/', {
+            method: 'POST',
+            body: OTPdata
+        }).then(result =>{
+            console.log('success:', result)
+        }).then(error =>{
+            console.log('error:', error)
+        })
+
+    }
+    onGoBack = () =>{
+        this.props.onshowlogin()
+    }
+    handleSubmit = (event) =>{
+        // reset password of the user
+        const formdata = new FormData()
+        // check whether the password and confirm password is same or not
+        if(this.state.newpassword === this.state.confirmpassword){
+            // check whether the entered OTP is correct or not
+            if(this.state.OTPcode === parseInt(this.state.otppassword)){
+                formdata.append('newpassword', this.state.newpassword)
+            fetch('http://127.0.0.1:8000/user/reset_password/'+localStorage.getItem('UserId'), {
+            method: 'POST',
+            Header: 'context-Type: application/json',
+            body: formdata
+            }
+            ).then((response) =>response.json())
+            .then((result)=>{
+                console.log('success', result)
+                this.onGoBack()
+            })
+            .catch((error)=>{
+                console.error('Error:',error)
+            })
+            }
+            else{
+                alert('Please enter correct OTP code.')
+            }
+        }
+        else{
+            alert('Confirmation password did not match')
+        }
+        event.preventDefault();
+
+    }
+    onChangeinput =(event)=>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+ 
+    render() {
+        const {otppassword, newpassword, confirmpassword} = this.state
+        return (
+            <div>
+                <form className = "signin-form" onSubmit = {this.handleSubmit}>
+                    <h2>Reset password</h2>
+                    <label>OTP Code</label>
+                    <input type = "number" name = "otppassword" value = {otppassword} required onChange = {this.onChangeinput}></input>
+                    <label>Password</label>
+                    <input type = "password" name = "newpassword" value = {newpassword} required onChange = {this.onChangeinput}></input>
+                    <label>Confirm Password</label>
+                    <input type = "password" name = "confirmpassword" value = {confirmpassword} required onChange = {this.onChangeinput}></input>
+                    <div className = 'btn-section'>
+                        <button type = "submit">submit</button>
+                        <button type = 'button' onClick = {this.onGoBack}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
--- final-project-react/src/Authentication/UserLogin.js
+++ final-project-react/src/Authentication/UserLogin.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './UserLogin.css'
+import ResetPassword from './ResetPassword'
 
 export default class UserLogin extends Component {
     state = {
         username: '',
         password: '',
+        loginpage: true,
     }
     jumpSignup = () =>{
         this.props.showsignup()
@@ -41,22 +43,44 @@ export default class UserLogin extends Component {
             [event.target.name]: event.target.value
         })
     }
+    onResetPassword = () =>{
+        if(this.state.loginpage === true){
+            this.setState({
+                loginpage: false
+            })
+        }
+        else{
+            this.setState({
+                loginpage: true
+            })
+        }
+    }
+    onShowLogin = () =>{
+        this.setState({
+            loginpage: true
+        })
+    }
     render() {
 
         console.log('local token : ',localStorage.getItem('token'))
         const {username, password} = this.state
         return (
             <div>
-                <form className = "signin-form" onSubmit = {this.handleSubmit}>
-                    <h2>User Login</h2>
-                    <label>Username</label>
-                    <input type = "text" name = "username" value = {username} required onChange = {this.onChangeinput}></input>
-                    <label>Password</label>
-                    <input type = "password" name = "password" value = {password} required onChange = {this.onChangeinput}></input>
-                    <a href = "https://www.google.com/"><p className = "forget-pass">Forgot password? reset password</p></a>
-                    <button type = "submit">Login</button>
-                    <p className = "create-acc" onClick = {this.jumpSignup}>Don't have account? Create one</p>
-                </form>
+                {
+                    (this.state.loginpage === true)?
+                    <form className = "signin-form" onSubmit = {this.handleSubmit}>
+                        <h2>User Login</h2>
+                        <label>Username</label>
+                        <input type = "text" name = "username" value = {username} required onChange = {this.onChangeinput}></input>
+                        <label>Password</label>
+                        <input type = "password" name = "password" value = {password} required onChange = {this.onChangeinput}></input>
+                        <p className = "forget-pass" onClick = {this.onResetPassword}>Forgot password? reset password</p>
+                        <button type = "submit">Login</button>
+                        <p className = "create-acc" onClick = {this.jumpSignup}>Don't have account? Create one</p>
+                    </form>
+                    :<ResetPassword onshowlogin = {this.onShowLogin}></ResetPassword>
+
+                }
             </div>
         )
     }
